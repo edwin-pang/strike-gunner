383bcd8ed2b61eb911e5aeabb113b25508b98733
Jhanf932 <113398827+Jhanf932@users.noreply.github.com>
2024-02-08 14:53:31 -0700

    Update MODEL.C

--- MODEL.C
+++ MODEL.C
@@ -3,9 +3,7 @@
 /*The location for things is going to represent the top left corner of their bitmap*/
 /*for now lets imagine that x = 0-50, and x = 590-640 are not inside the playable area (this is the side panel where health and stuff will be)*/
 /*no ship cannot fly below y = 368*/
-#define LEFT_BOUND   50
-#define RIGHT_BOUND  590
-#define PLAYER_WIDTH 32
+
 void move_player (PlayerShip player) {
     if (player.hor_dir == 1 && player.x < (RIGHT_BOUND - PLAYER_WIDTH)){
         player.x += 1;      /*move player one to the right if that direction is inputted, and it still has room to move right*/
