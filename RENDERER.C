0479d1fdcbc82b65a695fd824a8865688f358280
jhanf932 <jhanf932@mtroyal.ca>
2024-02-15 19:16:29 -0700

    some fixes

--- RENDERER.C
+++ RENDERER.C
@@ -2,7 +2,9 @@
 #include "RASTER.H"
 #include <osbind.h>
 
+
 void render(Model *model){
     void *base = Physbase();
-    clear_screen(base);
+    clear_screen(base);         /*clear entire screen*/
+    plot_bitmap_32(base,model->ship[0].position.x,model->ship[0].position.y,player_ship, SHIP_HEIGHT);
 }
