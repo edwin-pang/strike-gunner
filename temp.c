e271c6a01672d9cf8712e6745828170eb1b399c7
jhanf932 <jhanf932@mtroyal.ca>
2024-02-16 11:54:02 -0700

    might move this function for now

---
+++ temp.c
@@ -0,0 +1,36 @@
+
+
+void on_playership_move(Model *model, int player){
+  PlayerShip *curr_ship = &(model->ship[player]);
+
+  switch (curr_ship->hor_dir)
+  {
+  case RIGHT_KEY:                                 /* not too sure how hardware inputs work yet*/
+    /* move right calling move_player */          /* just assigned each "key" to a number in TYPES.H*/
+    move_player(curr_ship);
+    break;
+
+  case LEFT_KEY:
+    /* move left calling move_player*/
+    move_player(curr_ship);
+    break;
+  }
+
+  switch (curr_ship->ver_dir)
+  {
+  case UP_KEY:
+    /* move up calling move_player*/
+    move_player(curr_ship);
+    break;
+  
+  case DOWN_KEY:
+    /* move down calling move_player*/
+    move_player(curr_ship);
+    break;
+  }
+}
+
+void move_up_cancel(PlayerShip *player){
+  
+}
+
