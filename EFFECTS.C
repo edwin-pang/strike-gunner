821b12b164c7560cf7d76d3b15c0ae471cbc3d34
Edwin Pang <95311622+edwin-pang@users.noreply.github.com>
2024-03-15 21:47:11 -0600

    bleh

--- EFFECTS.C
+++ EFFECTS.C
@@ -1,27 +1,17 @@
 #include "EFFECTS.H"
 #include "PSG.H"
 void play_explosion(){
-    enable_channel(CHANNEL_A, FALSE, TRUE);
-    enable_channel(CHANNEL_B, FALSE, TRUE);
     enable_channel(CHANNEL_C, FALSE, TRUE);
-
-    set_volume(CHANNEL_A, 0x10);
-    set_volume(CHANNEL_B, 0x10);
     set_volume(CHANNEL_C, 0x10);
     set_noise(0x1F);
     set_envelope(0x00,0x0038);
-
+    set_volume(CHANNEL_C, 0);
 }
 
 void play_bullet(){
-    enable_channel(CHANNEL_A, FALSE, TRUE);
     enable_channel(CHANNEL_B, FALSE, TRUE);
-    enable_channel(CHANNEL_C, FALSE, TRUE);
-
-    set_volume(CHANNEL_A, 0x10);
     set_volume(CHANNEL_B, 0x10);
-    set_volume(CHANNEL_C, 0x10);
     set_noise(0x0F);
     set_envelope(0x00,0x0010);
-
+    set_volume(CHANNEL_B, 0);
 }
--- STRIKE.C
+++ STRIKE.C
@@ -6,10 +6,14 @@
 #include <osbind.h>
 #include "STRIKE.H"
 #include "RANDOM.H"
+#include "MUSIC.H"
+
 char buff[32256];
 Model model;
-char *get_base(char *second_buffer){
-    char *base;
+UINT32 duration = QUARTER_NOTE;
+
+UINT8 *get_base8(UINT8 *second_buffer){
+    UINT8 *base;
     UINT16 difference;
     base = second_buffer;
     difference = (int)base;
@@ -20,10 +24,8 @@ char *get_base(char *second_buffer){
 
 
 int main(){
-    char *base1 = Physbase();
-    char *base2 = get_base(buff);
-    char *active_base = base1;
-    char *inactive_base = base2;
+    UINT8 *active_base = base1;
+    UINT8 *inactive_base = base2;
     UINT8 prev_hor;
     UINT8 prev_ver;
 
@@ -41,6 +43,8 @@ int main(){
     render(model_ptr, active_base);
     render(model_ptr, base2);
     timeThen = get_time();
+    start_music();
+
     while(model_ptr->quit_game == FALSE){
         timeNow = get_time();
         timeElapsed = timeNow - timeThen;
@@ -53,6 +57,7 @@ int main(){
             }
         }
         else{
+            update_music(duration);
             sync_event_check();
             /*printf("%d %d %d %d %d %d %d %d %d %d \n", model_ptr->enemies[0].prev_y,model_ptr->enemies[0].position.y, model_ptr->enemies[1].prev_y,model_ptr->enemies[1].position.y,model_ptr->enemies[2].prev_y,model_ptr->enemies[2].position.y,model_ptr->enemies[3].prev_y,model_ptr->enemies[3].position.y,model_ptr->enemies[4].prev_y,model_ptr->enemies[4].position.y);*/
             render_moveables(model_ptr, active_base);
@@ -77,5 +82,6 @@ int main(){
     if (base2 = active_base){
         Setscreen(-1, base1, -1);
     }
+        set_volume(CHANNEL_A, 0);
     return 0;
 }
