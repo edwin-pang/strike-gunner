a508d54ff72d245d0b6dfa6dfb638781a5734f71
jhanf932 <jhanf932@mtroyal.ca>
2024-03-15 22:07:25 -0600

    game is completly done

--- EFFECTS.C
+++ EFFECTS.C
@@ -5,7 +5,7 @@ void play_explosion(){
     set_volume(CHANNEL_C, 0x10);
     set_noise(0x1F);
     set_envelope(0x00,0x0038);
-    set_volume(CHANNEL_C, 0);
+    /*set_volume(CHANNEL_C, 0);*/
 }
 
 void play_bullet(){
@@ -13,5 +13,5 @@ void play_bullet(){
     set_volume(CHANNEL_B, 0x10);
     set_noise(0x0F);
     set_envelope(0x00,0x0010);
-    set_volume(CHANNEL_B, 0);
+    /*set_volume(CHANNEL_B, 0);/*
 }
