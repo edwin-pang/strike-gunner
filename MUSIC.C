acf9e2ba2a99266020181abab9547c133f5b3415
Edwin Pang <95311622+edwin-pang@users.noreply.github.com>
2024-03-19 00:02:36 -0600

    added notes (still some bugs with music for some reason)

--- MUSIC.C
+++ MUSIC.C
@@ -2,22 +2,47 @@
 #include "TST_MUS.H"
 
 const Note song[] = {
-    {A3, QUARTER_NOTE},
-    {G4, QUARTER_NOTE},
-    {F3, QUARTER_NOTE},
-    {C3, QUARTER_NOTE},
-    {C3, QUARTER_NOTE},
-    {F4, QUARTER_NOTE},
-    {G4, QUARTER_NOTE},
-    {F4, QUARTER_NOTE},
-    {G4, QUARTER_NOTE},
-    {F4, QUARTER_NOTE},
-    {C3, QUARTER_NOTE},
-    {C4, QUARTER_NOTE},
-    {C4, QUARTER_NOTE},
-    {C3, QUARTER_NOTE},
+
+    {C4, SIXTEENTH_NOTE},
+    {C4, SIXTEENTH_NOTE},
+    {C4, SIXTEENTH_NOTE},
+    {C4, SIXTEENTH_NOTE},
+
     {A4, QUARTER_NOTE},
-    {G4, QUARTER_NOTE}
+    {C4, SIXTEENTH_NOTE},
+    {C4, SIXTEENTH_NOTE},
+    {C4, SIXTEENTH_NOTE},
+
+    {C4, SIXTEENTH_NOTE},
+    {A4, QUARTER_NOTE},
+    {C4, SIXTEENTH_NOTE},
+    {C4, SIXTEENTH_NOTE},
+
+    {C4, SIXTEENTH_NOTE},
+    {C4, SIXTEENTH_NOTE},
+    {A4, QUARTER_NOTE},
+    {C4, SIXTEENTH_NOTE},
+
+    {F2, HALF_NOTE},
+    {C2, QUARTER_NOTE},
+    {B2, SIXTEENTH_NOTE},
+    {C2, QUARTER_NOTE},
+
+    {F3, HALF_NOTE},
+    {C3, QUARTER_NOTE},
+    {B3, SIXTEENTH_NOTE},
+    {C3, QUARTER_NOTE},
+
+    {F4, HALF_NOTE},
+    {C4, QUARTER_NOTE},
+    {B4, SIXTEENTH_NOTE},
+    {C4, QUARTER_NOTE},
+
+    {F2, HALF_NOTE},
+    {C2, QUARTER_NOTE},
+    {B2, SIXTEENTH_NOTE},
+    {C2, QUARTER_NOTE},
+
 };
 
 int SONG_SZ = sizeof(song) / sizeof(song[0]);
