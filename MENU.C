e4871c152b00e24f1b91825e354464d079a8a9c5
Edwin Pang <epang758@mtroyal.ca>
2024-03-21 13:48:30 -0600

    added first little bit of menu screen

--- MENU.C
+++ MENU.C
@@ -14,23 +14,41 @@ int main(){
         plot_horizontal_line((UINT32*) base, 0, i, 640, 1);
     }*/
 
-    /* S */
+    /* STRIKE */
     plot_bitmap_32((UINT32 *)base, 10, 40, s_32, 32);
-
-    /* T */
     plot_bitmap_32((UINT32 *)base, 50, 40, t_32, 32);
-
-    /* R */
     plot_bitmap_32((UINT32 *)base, 100, 40, r_32, 32);
-
-    /* I */
     plot_bitmap_32((UINT32 *)base, 150, 40, i_32, 32);
-
-    /* K */
     plot_bitmap_32((UINT32 *)base, 200, 40, k_32, 32);
-
-    /* E */
     plot_bitmap_32((UINT32 *)base, 250, 40, e_32, 32);
+
+    /* GUNNER */
+    plot_bitmap_32((UINT32 *)base, 350, 40, g_32, 32);
+    plot_bitmap_32((UINT32 *)base, 400, 40, u_32, 32);
+    plot_bitmap_32((UINT32 *)base, 450, 40, n_32, 32);
+    plot_bitmap_32((UINT32 *)base, 500, 40, n_32, 32);
+    plot_bitmap_32((UINT32 *)base, 550, 40, e_32, 32);
+    plot_bitmap_32((UINT32 *)base, 600, 40, r_32, 32);
+
+    /* Player Ship */
+    plot_bitmap_32 ((UINT32 *)base, 300, 40, player_ship, 32);
+
+    /* Underline */
+    plot_horizontal_line((UINT32 *)base, 35, 80, 600, 1);
+    plot_horizontal_line((UINT32 *)base, 35, 81, 600, 1);
+    plot_horizontal_line((UINT32 *)base, 35, 82, 600, 1);
+    /* TO FIX ITS BUGGY */
+
+    /* 1P */
+    plot_bitmap_16((UINT16 *)base, 240, 200, one_16, 16);
+    plot_bitmap_16((UINT16 *)base, 260, 200, p_16, 16);
+
+    /* START */
+    plot_bitmap_16((UINT16 *)base, 300, 200, s_16, 16);
+    plot_bitmap_16((UINT16 *)base, 320, 200, t_16, 16);
+    plot_bitmap_16((UINT16 *)base, 340, 200, a_16, 16);
+    plot_bitmap_16((UINT16 *)base, 360, 200, r_16, 16);
+    plot_bitmap_16((UINT16 *)base, 380, 200, t_16, 16);
 
     /* WORK IN PROGRESS */
 
